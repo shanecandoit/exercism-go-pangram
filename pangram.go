2e4aa6dcc54e8ed97241c817b704f7f9bad9559a
shane <shanecandoit@gmail.com>
2019-12-30 15:26:46 -0600

    tests pass. use map of char to bool.

--- pangram.go
+++ pangram.go
@@ -1,1 +1,36 @@
 package pangram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsPangram is a sentence using every letter of the alphabet at least once.
+func IsPangram(sentence string) (ans bool) {
+
+	letters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	charList := strings.Split(letters, "")
+	charMap := make(map[string]bool, 26)
+
+	// make a map of all ascii chars
+	for _, ch := range charList {
+		charMap[ch] = false
+	}
+	// fmt.Println("charMap", charMap)
+
+	// set map to true if ch in sentence
+	for _, ch := range sentence {
+		ch = unicode.ToUpper(ch)
+		charMap[string(ch)] = true
+	}
+	// fmt.Println("charMap", charMap)
+
+	// if a single false exists (not in sentence, bail)
+	for _, ok := range charMap {
+		if ok == false {
+			return false
+		}
+	}
+
+	return true
+}
